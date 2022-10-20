2d007d82f9eb418ffbd760aeb9ba8e5be0261b1a
juvairiasabir <juvairiasabir123@gmail.com>
2022-10-20 09:50:35 +0530

    Done with the form

---
+++ demo-app/src/components/bookacall.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+
+export const bookacall = () => {
+    return (
+        <>
+        <h1>Hey There!</h1>
+
+        </>
+    )
+}
+export default bookacall
